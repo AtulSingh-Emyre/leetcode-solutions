b69d62a821320b7d7e231bf0e5570487d7231a5f
Atul Singh <59884618+AtulSingh-Emyre@users.noreply.github.com>
2026-05-31 07:57:13 +0530

    Time: 26 ms (81.52%), Space: 107.8 MB (7.85%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
+        int m = asteroids.length-1;
         for(int a: asteroids) {
             if(mass<a) return false;
+            if(mass>=asteroids[m]) return true;
             if(mass+a<mass) mass = Integer.MAX_VALUE;
             else mass+=a;
         }
